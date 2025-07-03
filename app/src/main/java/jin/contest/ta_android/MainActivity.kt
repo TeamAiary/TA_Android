a56785dfaf77595c65378f5de30fc8cdf357ad5b
ChanWooong <oco466kr@knu.ac.kr>
2025-07-04 00:22:19 +0900

    fix:실행 관련 오류 수정 (실행 즉시 꺼지는 증상 해결)

--- app/src/main/java/jin/contest/ta_android/MainActivity.kt
+++ app/src/main/java/jin/contest/ta_android/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import jin.contest.ta_android.databinding.ActivityMainBinding
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,7 +22,9 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        //val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navHostFragment.navController
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
