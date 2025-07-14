c050d33133d270ca10f65a43215a900aea1b1926
ChanWooong <oco466kr@knu.ac.kr>
2025-07-15 04:31:26 +0900

    feat #14 : 전체 디자인 조정

--- app/src/main/java/jin/contest/ta_android/ui/mypage/MyPageViewModel.kt
+++ app/src/main/java/jin/contest/ta_android/ui/mypage/MyPageViewModel.kt
@@ -6,8 +6,4 @@ import androidx.lifecycle.ViewModel
 
 class MyPageViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is mypage Fragment"
-    }
-    val text: LiveData<String> = _text
 }
